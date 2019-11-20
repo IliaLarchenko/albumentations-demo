6e61d26e26a5eeac2d7e78f7b74ce55ef45cb991
Ilia Larchenko <ilia.larchenko@gmail.com>
2019-11-20 22:47:46 +0300

    formatting

--- src/control.py
+++ src/control.py
@@ -3,32 +3,43 @@ import streamlit as st
 
 def select_int_interval(param_name, limits_list, defaults, **kwargs):
     st.sidebar.subheader(param_name)
-    min_max_interval = st.sidebar.slider('', limits_list[0], limits_list[1], defaults, key = hash(param_name))
+    min_max_interval = st.sidebar.slider(
+        "", limits_list[0], limits_list[1], defaults, key=hash(param_name)
+    )
     return min_max_interval
 
 
-def select_several_ints(param_name, subparam_names, limits_list, defaults_list, **kwargs):
+def select_several_ints(
+    param_name, subparam_names, limits_list, defaults_list, **kwargs
+):
     st.sidebar.subheader(param_name)
     result = []
     assert len(limits_list) == len(defaults_list)
     assert len(subparam_names) == len(defaults_list)
-    
+
     for name, limits, defaults in zip(subparam_names, limits_list, defaults_list):
-        result.append(st.sidebar.slider(name, limits[0], limits[1], defaults, key = hash(param_name + name)))
+        result.append(
+            st.sidebar.slider(
+                name, limits[0], limits[1], defaults, key=hash(param_name + name)
+            )
+        )
     return tuple(result)
 
+
 def select_several_RGB(param_name, **kwargs):
-    result = select_several_ints(param_name, 
-                                 subparam_names = ["Red", "Green", "Blue"], 
-                                 limits_list = [[0, 255],[0, 255],[0, 255]],
-                                 defaults_list = [0, 0, 0])
+    result = select_several_ints(
+        param_name,
+        subparam_names=["Red", "Green", "Blue"],
+        limits_list=[[0, 255], [0, 255], [0, 255]],
+        defaults_list=[0, 0, 0],
+    )
     return tuple(result)
 
 
 def select_radio(param_name, options_list, **kwargs):
     st.sidebar.subheader(param_name)
-    result = st.sidebar.radio('', options_list)
-    if isinstance(options_list[0],str):
+    result = st.sidebar.radio("", options_list)
+    if isinstance(options_list[0], str):
         result = '"' + result + '"'
     return result
 
@@ -39,13 +50,11 @@ def select_checkbox(param_name, defaults, **kwargs):
     return result
 
 
-
 # dict from param name to function showing this param
 param2func = {
-    'int_interval': select_int_interval,
-    'several_ints': select_several_ints,
-    'radio': select_radio,
-    'rgb': select_several_RGB,
-    'checkbox': select_checkbox
-
+    "int_interval": select_int_interval,
+    "several_ints": select_several_ints,
+    "radio": select_radio,
+    "rgb": select_several_RGB,
+    "checkbox": select_checkbox,
 }
--- src/main.py
+++ src/main.py
@@ -9,56 +9,65 @@ import streamlit as st
 from control import *
 
 
-def load_image(image_name, path_to_folder = '../images'):
+def load_image(image_name, path_to_folder="../images"):
     path_to_image = os.path.join(path_to_folder, image_name)
     image = cv2.imread(path_to_image)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 
-st.title('Demo of Albumentations transforms')
+st.title("Demo of Albumentations transforms")
 
 
 # selecting the image
-path_to_images = 'images'
-image_names_list = [x for x in os.listdir(path_to_images) if x[-3:] in ['jpg', 'peg', 'png']]
-image_name = st.sidebar.selectbox('Select an image:', image_names_list)
+path_to_images = "images"
+image_names_list = [
+    x for x in os.listdir(path_to_images) if x[-3:] in ["jpg", "peg", "png"]
+]
+image_name = st.sidebar.selectbox("Select an image:", image_names_list)
 image = load_image(image_name, path_to_images)
 
 
 # selecting the transformation
-path_to_config = 'configs/augmentations.json'
-with open(path_to_config, 'r') as config_file:
+path_to_config = "configs/augmentations.json"
+with open(path_to_config, "r") as config_file:
     augmentations = json.load(config_file)
-transform_name = st.sidebar.selectbox('Select a transformation:', sorted(list(augmentations.keys())))
+transform_name = st.sidebar.selectbox(
+    "Select a transformation:", sorted(list(augmentations.keys()))
+)
 transform_params = augmentations[transform_name]
 
 
 # show the transform options
 if len(transform_params) == 0:
-    st.sidebar.text(transform_name + ' transform has no parameters')
+    st.sidebar.text(transform_name + " transform has no parameters")
 else:
     for param in transform_params:
-        param['value'] = param2func[param['type']](**param)
-        
-        
-params_string = ", ".join([param['param_name'] + '=' + str(param['value']) for param in transform_params] + ['p=1.0'])
-params_string = '(' + params_string + ')'
+        param["value"] = param2func[param["type"]](**param)
+
+
+params_string = ", ".join(
+    [param["param_name"] + "=" + str(param["value"]) for param in transform_params]
+    + ["p=1.0"]
+)
+params_string = "(" + params_string + ")"
 
 st.text(transform_name + params_string)
-st.text('Press R to update')
-exec('transform = A.' + transform_name + params_string)
-st.image([image, transform(image = image)['image']], 
-         caption = ['Original image', 'Transformed image'],
-         width = 320)
+st.text("Press R to update")
+exec("transform = A." + transform_name + params_string)
+st.image(
+    [image, transform(image=image)["image"]],
+    caption=["Original image", "Transformed image"],
+    width=320,
+)
 
-st.subheader('Docstring:')
+st.subheader("Docstring:")
 st.text(str(transform.__doc__))
 
 
-st.text('')
-st.text('')
-st.subheader('Credentials:')
-st.text('Source: https://github.com/IliaLarchenko/albumentations-demo')
-st.text('Albumentations library: https://github.com/albumentations-team/albumentations')
-st.text('Image Source: https://www.pexels.com/royalty-free-images/')
+st.text("")
+st.text("")
+st.subheader("Credentials:")
+st.text("Source: https://github.com/IliaLarchenko/albumentations-demo")
+st.text("Albumentations library: https://github.com/albumentations-team/albumentations")
+st.text("Image Source: https://www.pexels.com/royalty-free-images/")
