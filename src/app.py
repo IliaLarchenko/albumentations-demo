8cc587cc3028b6ca8b0dc797594cbc37f174b546
Ilia Larchenko <ilia.larchenko@gmail.com>
2020-01-06 00:24:20 +0300

    refactoring

--- src/app.py
+++ src/app.py
@@ -16,72 +16,78 @@ from visuals import (
     get_transormations_params,
 )
 
-# TODO: refactor all the new code
 
-# get CLI params: the path to images and image width
-path_to_images, width_original = get_arguments()
+def main():
+    # get CLI params: the path to images and image width
+    path_to_images, width_original = get_arguments()
 
-if not os.path.isdir(path_to_images):
-    st.title("There is no directory: " + path_to_images)
-else:
-    # select interface type
-    interface_type = st.sidebar.radio(
-        "Select the interface mode", ["Simple", "Professional"]
-    )
-
-    # select image
-    status, image = select_image(path_to_images, interface_type)
-    if status == 1:
-        st.title("Can't load image")
-    if status == 2:
-        st.title("Please, upload the image")
+    if not os.path.isdir(path_to_images):
+        st.title("There is no directory: " + path_to_images)
     else:
-        # image was loaded successfully
-        placeholder_params = get_placeholder_params(image)
-
-        # load the config
-        augmentations = load_augmentations_config(
-            placeholder_params, "configs/augmentations.json"
+        # select interface type
+        interface_type = st.sidebar.radio(
+            "Select the interface mode", ["Simple", "Professional"]
         )
 
-        # get list of transformations names
-        transform_names = select_transformations(augmentations, interface_type)
+        # select image
+        status, image = select_image(path_to_images, interface_type)
+        if status == 1:
+            st.title("Can't load image")
+        if status == 2:
+            st.title("Please, upload the image")
+        else:
+            # image was loaded successfully
+            placeholder_params = get_placeholder_params(image)
 
-        # get parameters for each transform
-        transforms = get_transormations_params(transform_names, augmentations)
-
-        try:
-            # apply the transformation to the image
-            data = A.ReplayCompose(transforms)(image=image)
-            error = 0
-        except ValueError:
-            error = 1
-            st.title(
-                "The error has occurred. Most probably you have passed wrong set of parameters. \
-            Check transforms that change the shape of image."
+            # load the config
+            augmentations = load_augmentations_config(
+                placeholder_params, "configs/augmentations.json"
             )
 
-        # proced only if everything is ok
-        if error == 0:
-            augmented_image = data["image"]
-            # show title
-            st.title("Demo of Albumentations")
+            # get the list of transformations names
+            transform_names = select_transformations(augmentations, interface_type)
 
-            # show the images
-            width_transformed = int(
-                width_original / image.shape[1] * augmented_image.shape[1]
-            )
+            # get parameters for each transform
+            transforms = get_transormations_params(transform_names, augmentations)
 
-            st.image(image, caption="Original image", width=width_original)
-            st.image(
-                augmented_image, caption="Transformed image", width=width_transformed
-            )
+            try:
+                # apply the transformation to the image
+                data = A.ReplayCompose(transforms)(image=image)
+                error = 0
+            except ValueError:
+                error = 1
+                st.title(
+                    "The error has occurred. Most probably you have passed wrong set of parameters. \
+                Check transforms that change the shape of image."
+                )
 
-            # random values used to get transformations
-            show_random_params(data, interface_type)
+            # proceed only if everything is ok
+            if error == 0:
+                augmented_image = data["image"]
+                # show title
+                st.title("Demo of Albumentations")
 
-            # print additional info
-            for transform in transforms:
-                show_docstring(transform)
-                st.code(str(transform))
-            show_credentials()
+                # show the images
+                width_transformed = int(
+                    width_original / image.shape[1] * augmented_image.shape[1]
+                )
+
+                st.image(image, caption="Original image", width=width_original)
+                st.image(
+                    augmented_image,
+                    caption="Transformed image",
+                    width=width_transformed,
+                )
+
+                # random values used to get transformations
+                show_random_params(data, interface_type)
+
+                # print additional info
+                for transform in transforms:
+                    show_docstring(transform)
+                    st.code(str(transform))
+                show_credentials()
+
+
+if __name__ == "__main__":
+    main()
